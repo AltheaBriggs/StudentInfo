0909dc76ceea3ad274483019aba08c36a5329a60
AltheaBriggs <a.briggs@sjasd.ca>
2016-11-03 10:30:46 -0500

    Student Info Assigment
    Assigment

---
+++ src/StudentInfo.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+
+/**
+ * @author A.briggs
+ *
+ */
+public class StudentInfo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 double FirstName;
+         double LastName;
+         double Grade;
+         double StudentID;
+         double Login;
+         double SchoolAverage;
+
+         System.out.println(FirstName = Convert.ToDouble(First Name.Text);
+         System.out.println(LastName = Convert.ToDouble(Last Name.Text);
+         System.out.println(Grade = Convert.ToDouble(Grade (9-12).Text);
+         System.out.println(StudentID = Convert.ToDouble(Student ID.Text);
+         System.out.println(SchoolAverage = Convert.ToDouble(School Average.Text);
+
+          
+
+         Object lbldisplay;
+		lbldisplay.Text:, "Question 1:",  "what is your first Name", "Question 2:", "what is your lastName", "Question 3:"," What grade are you in (9-12) ?", "Question 4:", "what is your student ID?" , "Question 5:",  " What is your login ?" ,  "Question 6: , What is your School Average :",SchoolAverage);
+		
+	}
+
+}
